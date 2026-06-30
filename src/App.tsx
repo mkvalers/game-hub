ba1206c578bcb669b64c06e481acfe4be546940e
mkvalers <mkvalers@gmail.com>
2026-06-30 19:33:04 +0800

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button, HStack } from "@chakra-ui/react";
+import { Tooltip } from "./components/ui/tooltip";
+import { ColorModeButton } from "@/components/ui/color-mode";
+import { toaster, Toaster } from "@/components/ui/toaster";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <HStack>
+      <Tooltip content="Delete item">
+        <Button>Delete</Button>
+      </Tooltip>
+      <ColorModeButton>Button</ColorModeButton>
+      <Button
+        onClick={() => toaster.create({ title: "Saved!", type: "success" })}
+      >
+        Trigger Toast
+      </Button>
+      <Toaster />
+    </HStack>
+  );
 }
 
-export default App
+export default App;
