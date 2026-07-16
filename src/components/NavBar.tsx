2afc71e0de8fc26f1e2fefd735abab547df68f71
mkvalers <mkvalers@gmail.com>
2026-07-16 17:03:50 +0800

    Fix navbar logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,18 @@ import { Box, HStack, Image, Stack, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeButton } from "./ui/color-mode";
 import SearchInput from "./SearchInput";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const location = useLocation();
   const navigate = useNavigate();
 
   return (
     <HStack width="100%">
-      <Box onClick={() => navigate(-1)}>
+      <Box
+        onClick={() => location.pathname !== "/" && navigate(-1)}
+        cursor={"pointer"}
+      >
         <Image src={logo} boxSize={12} flexShrink={0} />
       </Box>
       <Box flex="1" minWidth={0}>
